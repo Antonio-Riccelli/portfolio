c03693ec01bf305f927d368ec4c5b7dae269143d
Antonio Riccelli <anton.riccelli@gmail.com>
2022-02-27 20:08:14 +0000

    Added a new project and tweaked styling

--- src/Components/ProjectSection/index.js
+++ src/Components/ProjectSection/index.js
@@ -23,17 +23,24 @@ export default function ProjectSection() {
             </section>
 
             <section className={styles.projectWrapper}>
-                <a href="https://antonio-riccelli.github.io/pixel_art_maker/" className={styles.projectLink}><img src="https://raw.githubusercontent.com/Antonio-Riccelli/pixel_art_maker/main/img/demo.gif" alt="Markdown previewer" className={styles.projectImg}></img></a>
+                <a href="https://antonio-riccelli.github.io/pixel_art_maker/" className={styles.projectLink}><img src="https://raw.githubusercontent.com/Antonio-Riccelli/pixel_art_maker/main/img/demo.gif" alt="Pixel Art Maker" className={styles.projectImg}></img></a>
                 <p>Pixel Art Maker</p>
                 <p></p>
                 <span className={styles.tags}>#Javascript #CSS #HTML </span>
             </section>
 
             <section className={styles.projectWrapper}>
-                <a href="https://github.com/Antonio-Riccelli/js-roman-numeral-converter" className={styles.projectLink}><img src="https://github.com/Antonio-Riccelli/js-roman-numeral-converter/blob/main/public/images/demo4.gif?raw=true" alt="Markdown previewer" className={styles.projectImg}></img></a>
+                <a href="https://github.com/Antonio-Riccelli/js-roman-numeral-converter" className={styles.projectLink}><img src="https://github.com/Antonio-Riccelli/js-roman-numeral-converter/blob/main/public/images/demo4.gif?raw=true" alt="Roman Numeral Converter and Quote Generator" className={styles.projectImg}></img></a>
                 <p>Roman Numeral Converter & Quote Generator</p>
                 <p></p>
                 <span className={styles.tags}>#JavaScript #TailwindCSS #Node.JS #Express.JS #PostgreSQL </span>
+            </section>
+
+            <section className={styles.projectWrapper}>
+                <a href="https://25-5-reactclock.netlify.app/" className={styles.projectLink}><img src="https://raw.githubusercontent.com/Antonio-Riccelli/React-25-5-clock/main/demo.gif" alt="25 + 5 Clock" className={styles.projectImg}></img></a>
+                <p>25 + 5 Clock</p>
+                
+                <span className={styles.tags}>#React #CSS</span>
             </section>
 
 
